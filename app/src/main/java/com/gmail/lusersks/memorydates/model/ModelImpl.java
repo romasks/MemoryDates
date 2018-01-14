bf3f4a36658fd2798342f51abd04ac018ecddf2f
Raman Skaskevich <lusersks@gmail.com>
2018-01-14 14:30:22 +0300

    Created Api service inside the model

--- app/src/main/java/com/gmail/lusersks/memorydates/model/ModelImpl.java
+++ app/src/main/java/com/gmail/lusersks/memorydates/model/ModelImpl.java
@@ -5,6 +5,9 @@ import com.gmail.lusersks.memorydates.entity.HistoryEvent;
 
 import java.util.List;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
@@ -13,10 +16,18 @@ public class ModelImpl implements Model {
     private String day;
     private GithubService api;
 
-    ModelImpl(String month, String day, GithubService api) {
+    ModelImpl(String month, String day) {
         this.month = month;
         this.day = day;
-        this.api = api;
+        this.api = buildApiService();
+    }
+
+    private GithubService buildApiService() {
+        return new Retrofit.Builder().baseUrl("https://raw.githubusercontent.com/")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService.class);
     }
 
     @Override
